a5d2aa1a891c5982b857d5303b48984023390a60
miche <AhmedSA19980>
2026-01-16 19:46:04 +0300

    utill layer

---
+++ UtillsLayer/clsMemeType.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace UtillsLayer
+{
+    public class clsMemeType
+    {
+        public static string GetMimeType(string filePath)
+        {
+
+            var extension = Path.GetExtension(filePath).ToLowerInvariant();
+            return extension switch
+            {
+                ".jpg" or ".jpeg" => "image/jpeg",
+                ".png" => "image/png",
+                ".gif" => "image/gif",
+                _ => "application/octet-stream",
+            };
+        }
+
+    }
+}
