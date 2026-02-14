472a32dc7ccceca25619095b3524ff283b5ad818
miche <AhmedSA19980>
2026-02-14 21:55:17 +0300

    add user card control

---
+++ PropertyRenting/User/controls/ctrlUserCard.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace PropertyRenting.User.controls
+{
+    partial class ctrlUserCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ctrlUserCard));
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            pbPersonal = new System.Windows.Forms.PictureBox();
+            lblEmail = new System.Windows.Forms.Label();
+            lblPhone = new System.Windows.Forms.Label();
+            lblnationality = new System.Windows.Forms.Label();
+            lblAddress = new System.Windows.Forms.Label();
+            lblGender = new System.Windows.Forms.Label();
+            lblDateOfBirth = new System.Windows.Forms.Label();
+            lblLastname = new System.Windows.Forms.Label();
+            lblFirstname = new System.Windows.Forms.Label();
+            label10 = new System.Windows.Forms.Label();
+            label7 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            groupBox2 = new System.Windows.Forms.GroupBox();
+            lblClientId = new System.Windows.Forms.Label();
+            lblUsername = new System.Windows.Forms.Label();
+            label11 = new System.Windows.Forms.Label();
+            label12 = new System.Windows.Forms.Label();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pbPersonal).BeginInit();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(pbPersonal);
+            groupBox1.Controls.Add(lblEmail);
+            groupBox1.Controls.Add(lblPhone);
+            groupBox1.Controls.Add(lblnationality);
+            groupBox1.Controls.Add(lblAddress);
+            groupBox1.Controls.Add(lblGender);
+            groupBox1.Controls.Add(lblDateOfBirth);
+            groupBox1.Controls.Add(lblLastname);
+            groupBox1.Controls.Add(lblFirstname);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new System.Drawing.Point(14, 20);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(564, 407);
+            groupBox1.TabIndex = 46;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "PersonInfo";
+            // 
+            // pbPersonal
+            // 
+            pbPersonal.Image = (System.Drawing.Image)resources.GetObject("pbPersonal.Image");
+            pbPersonal.Location = new System.Drawing.Point(62, 68);
+            pbPersonal.Name = "pbPersonal";
+            pbPersonal.Size = new System.Drawing.Size(118, 122);
+            pbPersonal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pbPersonal.TabIndex = 62;
+            pbPersonal.TabStop = false;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblEmail.Location = new System.Drawing.Point(123, 351);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new System.Drawing.Size(24, 21);
+            lblEmail.TabIndex = 61;
+            lblEmail.Text = "??";
+            // 
+            // lblPhone
+            // 
+            lblPhone.AutoSize = true;
+            lblPhone.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblPhone.Location = new System.Drawing.Point(346, 288);
+            lblPhone.Name = "lblPhone";
+            lblPhone.Size = new System.Drawing.Size(24, 21);
+            lblPhone.TabIndex = 60;
+            lblPhone.Text = "??";
+            // 
+            // lblnationality
+            // 
+            lblnationality.AutoSize = true;
+            lblnationality.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblnationality.Location = new System.Drawing.Point(387, 234);
+            lblnationality.Name = "lblnationality";
+            lblnationality.Size = new System.Drawing.Size(24, 21);
+            lblnationality.TabIndex = 59;
+            lblnationality.Text = "??";
+            // 
+            // lblAddress
+            // 
+            lblAddress.AutoSize = true;
+            lblAddress.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblAddress.Location = new System.Drawing.Point(155, 312);
+            lblAddress.Name = "lblAddress";
+            lblAddress.Size = new System.Drawing.Size(24, 21);
+            lblAddress.TabIndex = 58;
+            lblAddress.Text = "??";
+            // 
+            // lblGender
+            // 
+            lblGender.AutoSize = true;
+            lblGender.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblGender.Location = new System.Drawing.Point(155, 273);
+            lblGender.Name = "lblGender";
+            lblGender.Size = new System.Drawing.Size(24, 21);
+            lblGender.TabIndex = 57;
+            lblGender.Text = "??";
+            // 
+            // lblDateOfBirth
+            // 
+            lblDateOfBirth.AutoSize = true;
+            lblDateOfBirth.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblDateOfBirth.Location = new System.Drawing.Point(153, 237);
+            lblDateOfBirth.Name = "lblDateOfBirth";
+            lblDateOfBirth.Size = new System.Drawing.Size(24, 21);
+            lblDateOfBirth.TabIndex = 56;
+            lblDateOfBirth.Text = "??";
+            // 
+            // lblLastname
+            // 
+            lblLastname.AutoSize = true;
+            lblLastname.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblLastname.Location = new System.Drawing.Point(346, 115);
+            lblLastname.Name = "lblLastname";
+            lblLastname.Size = new System.Drawing.Size(24, 21);
+            lblLastname.TabIndex = 55;
+            lblLastname.Text = "??";
+            // 
+            // lblFirstname
+            // 
+            lblFirstname.AutoSize = true;
+            lblFirstname.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblFirstname.Location = new System.Drawing.Point(343, 68);
+            lblFirstname.Name = "lblFirstname";
+            lblFirstname.Size = new System.Drawing.Size(24, 21);
+            lblFirstname.TabIndex = 54;
+            lblFirstname.Text = "??";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label10.Location = new System.Drawing.Point(64, 351);
+            label10.Name = "label10";
+            label10.Size = new System.Drawing.Size(57, 21);
+            label10.TabIndex = 53;
+            label10.Text = "Email:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label7.Location = new System.Drawing.Point(281, 288);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(63, 21);
+            label7.TabIndex = 52;
+            label7.Text = "Phone:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label6.Location = new System.Drawing.Point(280, 237);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(101, 21);
+            label6.TabIndex = 51;
+            label6.Text = "Nationality:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label5.Location = new System.Drawing.Point(62, 312);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(74, 21);
+            label5.TabIndex = 50;
+            label5.Text = "Address:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label4.Location = new System.Drawing.Point(62, 271);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(70, 21);
+            label4.TabIndex = 49;
+            label4.Text = "Gender:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label3.Location = new System.Drawing.Point(62, 234);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(84, 21);
+            label3.TabIndex = 48;
+            label3.Text = "D.OfBirth:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label2.Location = new System.Drawing.Point(252, 115);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(90, 21);
+            label2.TabIndex = 47;
+            label2.Text = "LastName:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label1.Location = new System.Drawing.Point(252, 68);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(92, 21);
+            label1.TabIndex = 46;
+            label1.Text = "FirstName:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(lblClientId);
+            groupBox2.Controls.Add(lblUsername);
+            groupBox2.Controls.Add(label11);
+            groupBox2.Controls.Add(label12);
+            groupBox2.Location = new System.Drawing.Point(21, 445);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new System.Drawing.Size(564, 85);
+            groupBox2.TabIndex = 47;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "ClientInfo";
+            groupBox2.Enter += groupBox2_Enter;
+            // 
+            // lblClientId
+            // 
+            lblClientId.AutoSize = true;
+            lblClientId.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblClientId.Location = new System.Drawing.Point(387, 43);
+            lblClientId.Name = "lblClientId";
+            lblClientId.Size = new System.Drawing.Size(24, 21);
+            lblClientId.TabIndex = 59;
+            lblClientId.Text = "??";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblUsername.Location = new System.Drawing.Point(225, 43);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new System.Drawing.Size(24, 21);
+            lblUsername.TabIndex = 58;
+            lblUsername.Text = "??";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label11.Location = new System.Drawing.Point(305, 43);
+            label11.Name = "label11";
+            label11.Size = new System.Drawing.Size(76, 21);
+            label11.TabIndex = 57;
+            label11.Text = "ClientID:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label12.Location = new System.Drawing.Point(134, 43);
+            label12.Name = "label12";
+            label12.Size = new System.Drawing.Size(94, 21);
+            label12.TabIndex = 56;
+            label12.Text = "UserName:";
+            // 
+            // ctrlUserCard
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.SystemColors.ControlLightLight;
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "ctrlUserCard";
+            Size = new System.Drawing.Size(588, 544);
+            Load += ctrlUserCard_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pbPersonal).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+       // private System.Windows.Forms.PictureBox pbPersonal;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblnationality;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblDateOfBirth;
+        private System.Windows.Forms.Label lblLastname;
+        private System.Windows.Forms.Label lblFirstname;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblClientId;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.PictureBox pbPersonal;
+    }
+}
