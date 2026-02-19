49701857c1cca0dd758dec44754807686118be2c
miche <AhmedSA19980>
2026-02-20 01:04:54 +0300

    add reservation form + generate stripe link

---
+++ PropertyRenting/Reservation/frmAddUpdateReservation.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace PropertyRenting.Reservation
+{
+    partial class frmAddUpdateReservation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dtpStartDate = new System.Windows.Forms.DateTimePicker();
+            dtpExpDate = new System.Windows.Forms.DateTimePicker();
+            label1 = new System.Windows.Forms.Label();
+            lblPropertyName = new System.Windows.Forms.Label();
+            lblClientName = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            lblPrice = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            lblBookId = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            btnReserve = new System.Windows.Forms.Button();
+            label2 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            label7 = new System.Windows.Forms.Label();
+            linkl1 = new System.Windows.Forms.LinkLabel();
+            SuspendLayout();
+            // 
+            // dtpStartDate
+            // 
+            dtpStartDate.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            dtpStartDate.Location = new System.Drawing.Point(263, 236);
+            dtpStartDate.MinDate = new System.DateTime(2025, 4, 26, 0, 0, 0, 0);
+            dtpStartDate.Name = "dtpStartDate";
+            dtpStartDate.Size = new System.Drawing.Size(200, 25);
+            dtpStartDate.TabIndex = 0;
+            // 
+            // dtpExpDate
+            // 
+            dtpExpDate.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            dtpExpDate.Location = new System.Drawing.Point(263, 286);
+            dtpExpDate.MinDate = new System.DateTime(2025, 4, 26, 0, 0, 0, 0);
+            dtpExpDate.Name = "dtpExpDate";
+            dtpExpDate.Size = new System.Drawing.Size(200, 25);
+            dtpExpDate.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label1.Location = new System.Drawing.Point(131, 140);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(126, 21);
+            label1.TabIndex = 2;
+            label1.Text = "PropertyName:";
+            // 
+            // lblPropertyName
+            // 
+            lblPropertyName.AutoSize = true;
+            lblPropertyName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblPropertyName.Location = new System.Drawing.Point(252, 140);
+            lblPropertyName.Name = "lblPropertyName";
+            lblPropertyName.Size = new System.Drawing.Size(24, 21);
+            lblPropertyName.TabIndex = 3;
+            lblPropertyName.Text = "??";
+            // 
+            // lblClientName
+            // 
+            lblClientName.AutoSize = true;
+            lblClientName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblClientName.Location = new System.Drawing.Point(252, 188);
+            lblClientName.Name = "lblClientName";
+            lblClientName.Size = new System.Drawing.Size(24, 21);
+            lblClientName.TabIndex = 5;
+            lblClientName.Text = "??";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label4.Location = new System.Drawing.Point(131, 188);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(105, 21);
+            label4.TabIndex = 4;
+            label4.Text = "ClientName:";
+            // 
+            // lblPrice
+            // 
+            lblPrice.AutoSize = true;
+            lblPrice.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblPrice.Location = new System.Drawing.Point(509, 188);
+            lblPrice.Name = "lblPrice";
+            lblPrice.Size = new System.Drawing.Size(24, 21);
+            lblPrice.TabIndex = 7;
+            lblPrice.Text = "??";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label3.Location = new System.Drawing.Point(447, 188);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(52, 21);
+            label3.TabIndex = 6;
+            label3.Text = "Price:";
+            // 
+            // lblBookId
+            // 
+            lblBookId.AutoSize = true;
+            lblBookId.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            lblBookId.Location = new System.Drawing.Point(252, 101);
+            lblBookId.Name = "lblBookId";
+            lblBookId.Size = new System.Drawing.Size(24, 21);
+            lblBookId.TabIndex = 9;
+            lblBookId.Text = "??";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label5.Location = new System.Drawing.Point(131, 101);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(126, 21);
+            label5.TabIndex = 8;
+            label5.Text = "Reservation ID:";
+            // 
+            // btnReserve
+            // 
+            btnReserve.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            btnReserve.Location = new System.Drawing.Point(432, 338);
+            btnReserve.Name = "btnReserve";
+            btnReserve.Size = new System.Drawing.Size(101, 30);
+            btnReserve.TabIndex = 10;
+            btnReserve.Text = "Reserve";
+            btnReserve.UseVisualStyleBackColor = true;
+            btnReserve.Click += btnReserve_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label2.Location = new System.Drawing.Point(135, 238);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(69, 21);
+            label2.TabIndex = 11;
+            label2.Text = "St Date:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            label6.Location = new System.Drawing.Point(135, 288);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(72, 21);
+            label6.TabIndex = 12;
+            label6.Text = "Ex Date:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = System.Drawing.Color.White;
+            label7.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            label7.ForeColor = System.Drawing.Color.Teal;
+            label7.Location = new System.Drawing.Point(208, 23);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(242, 32);
+            label7.TabIndex = 13;
+            label7.Text = "Make A Reservation";
+            // 
+            // linkl1
+            // 
+            linkl1.AutoSize = true;
+            linkl1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            linkl1.Location = new System.Drawing.Point(447, 133);
+            linkl1.Name = "linkl1";
+            linkl1.Size = new System.Drawing.Size(31, 30);
+            linkl1.TabIndex = 14;
+            linkl1.TabStop = true;
+            linkl1.Text = "??";
+            linkl1.VisitedLinkColor = System.Drawing.Color.Red;
+            linkl1.LinkClicked += linkl1_LinkClicked;
+            // 
+            // frmAddUpdateReservation
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.SystemColors.ControlLightLight;
+            ClientSize = new System.Drawing.Size(662, 450);
+            Controls.Add(linkl1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label2);
+            Controls.Add(btnReserve);
+            Controls.Add(lblBookId);
+            Controls.Add(label5);
+            Controls.Add(lblPrice);
+            Controls.Add(label3);
+            Controls.Add(lblClientName);
+            Controls.Add(label4);
+            Controls.Add(lblPropertyName);
+            Controls.Add(label1);
+            Controls.Add(dtpExpDate);
+            Controls.Add(dtpStartDate);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            Name = "frmAddUpdateReservation";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "frmAddUpdateReservation";
+            Load += frmAddUpdateReservation_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
+        private System.Windows.Forms.DateTimePicker dtpExpDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPropertyName;
+        private System.Windows.Forms.Label lblClientName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBookId;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnReserve;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.LinkLabel linkl1;
+    }
+}
---
+++ PropertyRenting/Reservation/frmAddUpdateReservation.cs
@@ -0,0 +1,217 @@
+﻿using ApiClient;
+using Models;
+using PropertyRenting.ClassGlobal;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Reflection.Metadata.BlobBuilder;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PropertyRenting.Reservation
+{
+    public partial class frmAddUpdateReservation : Form
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enum enGendor { Male = 0, Female = 1 };
+        private enMode _Mode = enMode.AddNew;
+
+        private int _ReservationID;
+        private int _PropertyID;
+       
+        private string _ClientName;
+        private string _PropertyName;
+        private decimal _PropertyPrice;
+        private Models.PaymentSession webLink;
+       
+        public frmAddUpdateReservation(int PropertyID)
+        {
+            InitializeComponent();
+            _PropertyID = PropertyID;
+        }
+
+        public frmAddUpdateReservation(int reservationID, int PropertyID)
+        {
+            InitializeComponent();
+            _Mode = enMode.Update;
+            _ReservationID = reservationID;
+            _PropertyID = PropertyID;
+            _Mode = enMode.Update;
+        }
+
+      
+        private async Task <Models.Reservation> ActivateReservationDate() {
+
+          Models.Reservation ActiveDate =   await ApiClient.clsAPIFunctions<Models.Reservation>.GetAsync($"Reservation/getActiveReservationDate?PropertyId={_PropertyID}&stDate={dtpStartDate.Value.ToShortDateString()}&exDate={dtpExpDate.Value.ToShortDateString()}");
+        
+          return ActiveDate;
+        
+        }
+
+        private async Task<Models.Reservation> AddReserevation(Models.Reservation reservation) {
+
+            return await ApiClient.clsAPIFunctions<Models.Reservation>.PostAsync("Reservation/Add", reservation);
+
+        }
+
+        private async Task<Models.Reservation> UpdateReservation(int reservationId ,Models.Reservation reservation)
+        {
+            return   await ApiClient.clsAPIFunctions<Models.Reservation>.PutAsync($"Reservation/UpdateReservationDate?UpdateReservationById={reservationId}", reservation);
+        }
+        private async void btnReserve_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                
+               
+                Models.Reservation ResultResrvation;
+                Models.Reservation ActiveReservationDate =await ActivateReservationDate(); 
+
+                if (ActiveReservationDate != null)
+                {
+                    MessageBox.Show($"Selected Reservation Date Time {ActiveReservationDate.BeginDate}-{ActiveReservationDate.ExpiredDate} is Already Token !  ", "Info", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                Models.Reservation reservation = new Models.Reservation
+                {
+
+                    PropertyId = _PropertyID,
+                    BookedByClientId = clsGlobal.CurrentUser.ClientID,
+                    Price = _PropertyPrice,
+                    BeginDate = dtpStartDate.Value,
+                    ExpiredDate = dtpExpDate.Value,
+
+                };
+
+
+                if (_Mode == enMode.AddNew) ResultResrvation =await AddReserevation(reservation);
+                {
+                    reservation.BookID = _ReservationID;
+                    ResultResrvation = await UpdateReservation(_ReservationID , reservation);
+
+                }
+
+                _ReservationID = ResultResrvation.BookID;
+                var propertName =await GetPropertyNameByid(reservation.PropertyId) ; 
+                var Payment = new { PropertyName = propertName, Price = ResultResrvation.Price , BookId = ResultResrvation.BookID.ToString()  };
+                
+                if (_ReservationID != -1)
+                {
+                    string result = _Mode == enMode.AddNew ? "Created" : "Update";
+                    webLink = await ApiClient.clsAPIFunctions<Models.PaymentSession>.PostAsync("Payment/CreateCheckoutSessionAsync", Payment);
+                    if (webLink != null)
+                    {
+                        linkl1.Visible = true;
+                        linkl1.Text = "Pay";
+                        MessageBox.Show($"Reservation {result} Successfully, Confirm your payment in order to compeleted the process open the link (Pay)!", "Success", MessageBoxButtons.OK , MessageBoxIcon.Information);
+
+                    
+                      
+                    }
+                  
+
+                }
+                lblBookId.Text = ResultResrvation.BookID.ToString();        
+                _Mode = enMode.Update;
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+                clsGlobal.SaveToEventLog($"error{ex.Message}", System.Diagnostics.EventLogEntryType.Error);
+
+            }
+
+        }
+
+        private async Task<string> GetPropertyNameByid(int propertyId)
+        {
+           return  await ApiClient.clsAPIFunctions<string>.GetAsync("Property/GetPropertyNameByID?PropertyID=", propertyId);
+        }
+        private async Task<string> ClientName()
+        {
+           string  clientName =  await ApiClient.clsAPIFunctions<string>.GetAsync("Clients/GetClientUserNameById?id=", clsGlobal.CurrentUser.ClientID);
+            return clientName;
+        }
+
+        private async Task<string> GetPropertyName(int propertyId) 
+        {
+            string propertyName = await ApiClient.clsAPIFunctions<string>.GetAsync("Clients/GetClientUserNameById?id=", propertyId);
+            return propertyName;
+        }
+
+
+        private async Task<decimal> GetPropertyPrice(int propertyId)
+        {
+            decimal propertyPrice = await ApiClient.clsAPIFunctions<decimal>.GetAsync("Property/GetPropertyPriceByID?PropertyID=", propertyId);
+            return propertyPrice;
+        }
+
+
+        private async void frmAddUpdateReservation_Load(object sender, EventArgs e)
+        {
+           
+            dtpExpDate.MinDate = DateTime.Today.AddDays(2);
+            dtpStartDate.MinDate = DateTime.Today.AddDays(1);
+            linkl1.Visible = false;
+
+
+
+
+            _ClientName =await ClientName() ;
+
+            _PropertyName = await GetPropertyName(_PropertyID);  ;
+            _PropertyPrice =await GetPropertyPrice(_PropertyID) ;
+
+            lblClientName.Text = _ClientName;
+            lblPropertyName.Text = _PropertyName;
+            lblPrice.Text = _PropertyPrice.ToString();
+
+            if (_Mode == enMode.Update)
+            {
+                var Reservation = await GetReservation(_ReservationID);
+
+                lblBookId.Text = Reservation.BookID.ToString();
+                dtpStartDate.Text = Reservation.BeginDate.ToShortDateString();
+                dtpExpDate.Text = Reservation.ExpiredDate.ToShortDateString();
+            }
+
+        }
+
+        private async Task<Models.Reservation> GetReservation(int reservationId)
+        {
+            Models.Reservation reservation = await ApiClient.clsAPIFunctions<Models.Reservation>.GetAsync($"Reservation/GetReservationById?BookId={reservationId}");
+            return reservation;
+        }
+
+
+        private void linkl1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+          
+            try
+            {
+                
+                string url = webLink.url;
+                var psi = new ProcessStartInfo
+                {
+                    FileName =url,
+                    UseShellExecute = true   // <— IMPORTANT
+                };
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Could not open the link: {ex.Message}");
+            }
+        }
+    }
+}
