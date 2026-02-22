ea8b60e2cde739cfbc900f0db0eb1cd11846db02
miche <AhmedSA19980>
2026-02-22 22:32:50 +0300

    property display detail form is added

---
+++ PropertyRenting/Property/controls/ShowPropertyInfo.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace PropertyRenting.Property.controls
+{
+    partial class ctrlShowPropertyInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl4 = new System.Windows.Forms.Label();
+            lbl3 = new System.Windows.Forms.Label();
+            lbl2 = new System.Windows.Forms.Label();
+            lbl6 = new System.Windows.Forms.Label();
+            lbl9 = new System.Windows.Forms.Label();
+            lbl8 = new System.Windows.Forms.Label();
+            lbl7 = new System.Windows.Forms.Label();
+            lblDescription = new System.Windows.Forms.Label();
+            lblShowPrecentOff = new System.Windows.Forms.Label();
+            lblShowCity = new System.Windows.Forms.Label();
+            lblShowAddress = new System.Windows.Forms.Label();
+            lblShowPropertyType = new System.Windows.Forms.Label();
+            lblShowDesc = new System.Windows.Forms.Label();
+            lblBathroomsNo = new System.Windows.Forms.Label();
+            lblShowCountry = new System.Windows.Forms.Label();
+            lblShowPrice = new System.Windows.Forms.Label();
+            lblShowBedroomsNo = new System.Windows.Forms.Label();
+            lblName = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            gbBoxDescription = new System.Windows.Forms.GroupBox();
+            gbBoxDescription.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lbl4
+            // 
+            lbl4.AllowDrop = true;
+            lbl4.AutoSize = true;
+            lbl4.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lbl4.Location = new System.Drawing.Point(36, 230);
+            lbl4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbl4.Name = "lbl4";
+            lbl4.Size = new System.Drawing.Size(117, 21);
+            lbl4.TabIndex = 24;
+            lbl4.Text = "PropertyType:";
+            // 
+            // lbl3
+            // 
+            lbl3.AutoSize = true;
+            lbl3.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lbl3.Location = new System.Drawing.Point(79, 180);
+            lbl3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbl3.Name = "lbl3";
+            lbl3.Size = new System.Drawing.Size(74, 21);
+            lbl3.TabIndex = 23;
+            lbl3.Text = "Address:";
+            // 
+            // lbl2
+            // 
+            lbl2.AutoSize = true;
+            lbl2.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lbl2.Location = new System.Drawing.Point(113, 136);
+            lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbl2.Name = "lbl2";
+            lbl2.Size = new System.Drawing.Size(44, 21);
+            lbl2.TabIndex = 22;
+            lbl2.Text = "City:";
+            // 
+            // lbl6
+            // 
+            lbl6.AllowDrop = true;
+            lbl6.AutoSize = true;
+            lbl6.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lbl6.Location = new System.Drawing.Point(357, 95);
+            lbl6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbl6.Name = "lbl6";
+            lbl6.Size = new System.Drawing.Size(131, 21);
+            lbl6.TabIndex = 29;
+            lbl6.Text = "Bathroom\\s No:";
+            // 
+            // lbl9
+            // 
+            lbl9.AllowDrop = true;
+            lbl9.AutoSize = true;
+            lbl9.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lbl9.Location = new System.Drawing.Point(364, 234);
+            lbl9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbl9.Name = "lbl9";
+            lbl9.Size = new System.Drawing.Size(125, 21);
+            lbl9.TabIndex = 28;
+            lbl9.Text = "Bedroom\\s No:";
+            // 
+            // lbl8
+            // 
+            lbl8.AutoSize = true;
+            lbl8.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lbl8.Location = new System.Drawing.Point(432, 197);
+            lbl8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbl8.Name = "lbl8";
+            lbl8.Size = new System.Drawing.Size(52, 21);
+            lbl8.TabIndex = 27;
+            lbl8.Text = "Price:";
+            // 
+            // lbl7
+            // 
+            lbl7.AllowDrop = true;
+            lbl7.AutoSize = true;
+            lbl7.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lbl7.Location = new System.Drawing.Point(410, 147);
+            lbl7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbl7.Name = "lbl7";
+            lbl7.Size = new System.Drawing.Size(75, 21);
+            lbl7.TabIndex = 26;
+            lbl7.Text = "Country:";
+            // 
+            // lblDescription
+            // 
+            lblDescription.AutoSize = true;
+            lblDescription.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            lblDescription.Location = new System.Drawing.Point(7, 19);
+            lblDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblDescription.Name = "lblDescription";
+            lblDescription.Size = new System.Drawing.Size(102, 21);
+            lblDescription.TabIndex = 30;
+            lblDescription.Text = "Description:";
+            // 
+            // lblShowPrecentOff
+            // 
+            lblShowPrecentOff.AutoSize = true;
+            lblShowPrecentOff.BackColor = System.Drawing.Color.BlanchedAlmond;
+            lblShowPrecentOff.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            lblShowPrecentOff.Location = new System.Drawing.Point(62, 95);
+            lblShowPrecentOff.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblShowPrecentOff.Name = "lblShowPrecentOff";
+            lblShowPrecentOff.Size = new System.Drawing.Size(110, 21);
+            lblShowPrecentOff.TabIndex = 32;
+            lblShowPrecentOff.Text = "ShowDiscount";
+            // 
+            // lblShowCity
+            // 
+            lblShowCity.AutoSize = true;
+            lblShowCity.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lblShowCity.Location = new System.Drawing.Point(166, 136);
+            lblShowCity.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblShowCity.Name = "lblShowCity";
+            lblShowCity.Size = new System.Drawing.Size(31, 21);
+            lblShowCity.TabIndex = 33;
+            lblShowCity.Text = "???";
+            lblShowCity.Click += lblShowCity_Click;
+            // 
+            // lblShowAddress
+            // 
+            lblShowAddress.AutoSize = true;
+            lblShowAddress.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lblShowAddress.Location = new System.Drawing.Point(166, 180);
+            lblShowAddress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblShowAddress.Name = "lblShowAddress";
+            lblShowAddress.Size = new System.Drawing.Size(31, 21);
+            lblShowAddress.TabIndex = 34;
+            lblShowAddress.Text = "???";
+            // 
+            // lblShowPropertyType
+            // 
+            lblShowPropertyType.AllowDrop = true;
+            lblShowPropertyType.AutoSize = true;
+            lblShowPropertyType.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lblShowPropertyType.Location = new System.Drawing.Point(166, 230);
+            lblShowPropertyType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblShowPropertyType.Name = "lblShowPropertyType";
+            lblShowPropertyType.Size = new System.Drawing.Size(31, 21);
+            lblShowPropertyType.TabIndex = 35;
+            lblShowPropertyType.Text = "???";
+            // 
+            // lblShowDesc
+            // 
+            lblShowDesc.AutoSize = true;
+            lblShowDesc.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            lblShowDesc.Location = new System.Drawing.Point(107, 19);
+            lblShowDesc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblShowDesc.Name = "lblShowDesc";
+            lblShowDesc.Size = new System.Drawing.Size(38, 21);
+            lblShowDesc.TabIndex = 36;
+            lblShowDesc.Text = "????";
+            // 
+            // lblBathroomsNo
+            // 
+            lblBathroomsNo.AllowDrop = true;
+            lblBathroomsNo.AutoSize = true;
+            lblBathroomsNo.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lblBathroomsNo.Location = new System.Drawing.Point(496, 95);
+            lblBathroomsNo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblBathroomsNo.Name = "lblBathroomsNo";
+            lblBathroomsNo.Size = new System.Drawing.Size(31, 21);
+            lblBathroomsNo.TabIndex = 37;
+            lblBathroomsNo.Text = "???";
+            // 
+            // lblShowCountry
+            // 
+            lblShowCountry.AllowDrop = true;
+            lblShowCountry.AutoSize = true;
+            lblShowCountry.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lblShowCountry.Location = new System.Drawing.Point(498, 147);
+            lblShowCountry.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblShowCountry.Name = "lblShowCountry";
+            lblShowCountry.Size = new System.Drawing.Size(31, 21);
+            lblShowCountry.TabIndex = 38;
+            lblShowCountry.Text = "???";
+            // 
+            // lblShowPrice
+            // 
+            lblShowPrice.AutoSize = true;
+            lblShowPrice.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lblShowPrice.Location = new System.Drawing.Point(498, 197);
+            lblShowPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblShowPrice.Name = "lblShowPrice";
+            lblShowPrice.Size = new System.Drawing.Size(31, 21);
+            lblShowPrice.TabIndex = 39;
+            lblShowPrice.Text = "???";
+            // 
+            // lblShowBedroomsNo
+            // 
+            lblShowBedroomsNo.AllowDrop = true;
+            lblShowBedroomsNo.AutoSize = true;
+            lblShowBedroomsNo.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold);
+            lblShowBedroomsNo.Location = new System.Drawing.Point(498, 234);
+            lblShowBedroomsNo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblShowBedroomsNo.Name = "lblShowBedroomsNo";
+            lblShowBedroomsNo.Size = new System.Drawing.Size(31, 21);
+            lblShowBedroomsNo.TabIndex = 40;
+            lblShowBedroomsNo.Text = "???";
+            // 
+            // lblName
+            // 
+            lblName.AllowDrop = true;
+            lblName.AutoSize = true;
+            lblName.Font = new System.Drawing.Font("Microsoft PhagsPa", 15.75F, System.Drawing.FontStyle.Bold);
+            lblName.Location = new System.Drawing.Point(314, 42);
+            lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblName.Name = "lblName";
+            lblName.Size = new System.Drawing.Size(39, 27);
+            lblName.TabIndex = 42;
+            lblName.Text = "???";
+            // 
+            // label2
+            // 
+            label2.AllowDrop = true;
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Microsoft PhagsPa", 15.75F, System.Drawing.FontStyle.Bold);
+            label2.Location = new System.Drawing.Point(233, 42);
+            label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(76, 27);
+            label2.TabIndex = 41;
+            label2.Text = "Name:";
+            // 
+            // gbBoxDescription
+            // 
+            gbBoxDescription.Controls.Add(lblDescription);
+            gbBoxDescription.Controls.Add(lblShowDesc);
+            gbBoxDescription.Location = new System.Drawing.Point(73, 272);
+            gbBoxDescription.Name = "gbBoxDescription";
+            gbBoxDescription.Size = new System.Drawing.Size(537, 130);
+            gbBoxDescription.TabIndex = 43;
+            gbBoxDescription.TabStop = false;
+            // 
+            // ctrlShowPropertyInfo
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.SystemColors.ControlLightLight;
+            Controls.Add(gbBoxDescription);
+            Controls.Add(lblName);
+            Controls.Add(label2);
+            Controls.Add(lblShowBedroomsNo);
+            Controls.Add(lblShowPrice);
+            Controls.Add(lblShowCountry);
+            Controls.Add(lblBathroomsNo);
+            Controls.Add(lblShowPropertyType);
+            Controls.Add(lblShowAddress);
+            Controls.Add(lblShowCity);
+            Controls.Add(lblShowPrecentOff);
+            Controls.Add(lbl6);
+            Controls.Add(lbl9);
+            Controls.Add(lbl8);
+            Controls.Add(lbl7);
+            Controls.Add(lbl4);
+            Controls.Add(lbl3);
+            Controls.Add(lbl2);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "ctrlShowPropertyInfo";
+            Size = new System.Drawing.Size(629, 418);
+            Load += ctrlShowPropertyInfo_Load;
+            gbBoxDescription.ResumeLayout(false);
+            gbBoxDescription.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblShowPrecentOff;
+        private System.Windows.Forms.Label lblShowCity;
+        private System.Windows.Forms.Label lblShowAddress;
+        private System.Windows.Forms.Label lblShowPropertyType;
+        private System.Windows.Forms.Label lblShowDesc;
+        private System.Windows.Forms.Label lblBathroomsNo;
+        private System.Windows.Forms.Label lblShowCountry;
+        private System.Windows.Forms.Label lblShowPrice;
+        private System.Windows.Forms.Label lblShowBedroomsNo;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox gbBoxDescription;
+    }
+}
